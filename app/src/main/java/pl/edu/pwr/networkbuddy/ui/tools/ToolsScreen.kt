3477b1918663a715497d97179f916416995c085d
Kubagf <kubafg5@gmail.com>
2024-11-23 22:36:07 +0100

    Tools improvements and Port Scanner functionality

--- app/src/main/java/pl/edu/pwr/networkbuddy/ui/tools/ToolsScreen.kt
+++ app/src/main/java/pl/edu/pwr/networkbuddy/ui/tools/ToolsScreen.kt
@@ -1,7 +1,9 @@
 package pl.edu.pwr.networkbuddy.ui.tools
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +13,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
@@ -22,18 +27,24 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.net.InetAddress
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ToolsScreen() {
-    var query by rememberSaveable { mutableStateOf("") }
+    var host by rememberSaveable { mutableStateOf("") }
+    var port by rememberSaveable { mutableStateOf("") }
     var output by rememberSaveable { mutableStateOf("") }
-    var selectedOption by rememberSaveable { mutableStateOf<String?>(null) }
+    var selectedIndex by rememberSaveable { mutableStateOf(-1) }
+    val options = listOf("Ping", "Traceroute", "DNS lookup", "Port scan")
+
+    LaunchedEffect(selectedIndex) {
+        output = ""
+    }
 
     Scaffold(contentWindowInsets = WindowInsets(0.dp), topBar = {
         TopAppBar(title = {
@@ -41,65 +52,104 @@ fun ToolsScreen() {
                 text = "Tools", style = MaterialTheme.typography.titleLarge
             )
         })
-    }) { it ->
+    }, bottomBar = {
+        Button(
+            onClick = {
+                when (selectedIndex) {
+                    0 -> {
+                        output = "Pinging $host..."
+                        CoroutineScope(Dispatchers.IO).launch {
+                            output = executePing(host)
+                        }
+                    }
+
+                    1 -> {
+                        output = "Tracerouting $host..."
+                        CoroutineScope(Dispatchers.IO).launch {
+                            output = executeTraceroute(host)
+                        }
+                    }
+
+                    2 -> {
+                        output = "Looking up DNS for $host..."
+                        CoroutineScope(Dispatchers.IO).launch {
+                            output = executeDnsLookup(host)
+                        }
+                    }
+
+                    3 -> {
+                        output = "Scanning port $port on $host..."
+                        CoroutineScope(Dispatchers.IO).launch {
+                            output = executePortScan(host, port.toIntOrNull() ?: 0)
+                        }
+                    }
+                }
+            },
+            enabled = host.isNotEmpty() && selectedIndex >= 0,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                .fillMaxWidth()
+        ) {
+            Text("Execute")
+        }
+    }) {
         Box(
             Modifier
                 .fillMaxSize()
                 .padding(it)
         ) {
-            LazyColumn {
-                item {
-                    TextField(
-                        value = query,
-                        onValueChange = { query = it },
-                        label = { Text("IP address / hostname") },
-                        modifier = Modifier
-                            .padding(16.dp)
-                            .fillMaxWidth()
-                    )
-                }
-                item {
-                    RowButtons { selectedLabel ->
-                        selectedOption = selectedLabel
-                        output = "Loading..."
-                    }
-                }
-                item {
-                    Text(
-                        text = output, modifier = Modifier.padding(16.dp)
-                    )
-                }
-            }
-
-        }
-    }
-
-    LaunchedEffect(selectedOption, query) {
-        if (query.isNotBlank() && selectedOption != null) {
-            output = when (selectedOption) {
-                "Ping" -> executePing(query)
-                "Traceroute" -> executeTraceroute(query)
-                "DNS lookup" -> executeDnsLookup(query)
-                else -> "Invalid option"
-            }
-        }
-    }
-}
-
-@Composable
-fun RowButtons(onClick: (String) -> Unit) {
-    val options = listOf("Ping", "Traceroute", "DNS lookup")
-
-    Column(Modifier.padding(horizontal = 16.dp)) {
-        options.forEach { label ->
-            Button(
-                onClick = {
-                    onClick(label)
-                }, modifier = Modifier
-                    .padding(vertical = 4.dp)
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
                     .fillMaxWidth()
             ) {
-                Text(label)
+                SingleChoiceSegmentedButtonRow(modifier = Modifier.fillMaxWidth()) {
+                    options.forEachIndexed { index, label ->
+                        SegmentedButton(
+                            shape = SegmentedButtonDefaults.itemShape(
+                                index = index, count = options.size
+                            ),
+                            onClick = { selectedIndex = index },
+                            selected = index == selectedIndex
+                        ) {
+                            Text(label)
+                        }
+                    }
+                }
+                Row {
+                    TextField(
+                        value = host,
+                        onValueChange = { host = it },
+                        label = { Text("IP address / hostname") },
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .weight(2f)
+                    )
+                    if (selectedIndex == 3) {
+                        TextField(
+                            value = port,
+                            onValueChange = { port = it },
+                            label = { Text("Port") },
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .weight(1f)
+                        )
+                    }
+                }
+                LazyColumn(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.surfaceVariant)
+                        .fillMaxSize()
+                        .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+                ) {
+                    item {
+                        Text(
+                            text = output,
+                            modifier = Modifier.padding(8.dp),
+                            color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    }
+                }
             }
         }
     }
@@ -110,44 +160,35 @@ suspend fun executePing(host: String): String {
 }
 
 suspend fun executeTraceroute(host: String): String {
-    return withContext(Dispatchers.IO) {
-        val hops = mutableListOf<String>()
-        var reachedDestination = false
+    return try {
+        withContext(Dispatchers.IO) {
+            val hops = mutableListOf<String>()
+            var reachedDestination = false
 
-        for (ttl in 1..30) {
-            val command = "ping -c 1 -t $ttl $host"
-            val result = executeCommand(command)
-            val hopInfo = when {
-                result.contains("time=") -> {
-                    val time = result.substringAfter("time=").substringBefore(" ms").trim()
-                    "Hop $ttl: Reached destination at $host in $time ms"
+            for (ttl in 1..30) {
+                val command = "ping -c 1 -t $ttl $host"
+                val result = executeCommand(command)
+                val hopInfo = when {
+                    result.contains("Time to live exceeded") -> {
+                        val from = result.substringAfter("From ").substringBefore(":").trim()
+                        "Hop $ttl: $from"
+                    }
+
+                    else -> "Hop $ttl: *"
                 }
-
-                result.contains("Time to live exceeded") -> {
-                    val from = result.substringAfter("From ").substringBefore(":").trim()
-                    val time = executeCommand("ping -c 1 $from").substringAfter("time=")
-                        .substringBefore(" ms").trim()
-                    "Hop $ttl: $from, Time: $time ms"
+                hops.add(hopInfo)
+                if (result.contains("time=")) {
+                    reachedDestination = true
+                    break
                 }
-
-                result.contains("from") -> {
-                    val from = result.substringAfter("from ").substringBefore(":").trim()
-                    val time = result.substringAfter("time=").substringBefore(" ms").trim()
-                    "Hop $ttl: $from, Time: $time ms"
-                }
-
-                else -> "Hop $ttl: *"
             }
-            hops.add(hopInfo)
-            if (result.contains("time=")) {
-                reachedDestination = true
-                break
+            if (!reachedDestination) {
+                hops.add("Destination not reached within 30 hops")
             }
+            hops.joinToString(separator = "\n")
         }
-        if (!reachedDestination) {
-            hops.add("Destination not reached within 30 hops")
-        }
-        hops.joinToString(separator = "\n")
+    } catch (e: Exception) {
+        "Error executing traceroute"
     }
 }
 
@@ -158,7 +199,21 @@ suspend fun executeDnsLookup(host: String): String {
         }
         "Host: ${address.hostName}\nIP Address: ${address.hostAddress}"
     } catch (e: Exception) {
-        "DNS lookup error: ${e.message}"
+        "Error executing DNS lookup"
+    }
+}
+
+suspend fun executePortScan(host: String, port: Int): String {
+    return try {
+        withContext(Dispatchers.IO) {
+            val socket = java.net.Socket()
+            socket.use {
+                it.connect(java.net.InetSocketAddress(host, port), 1000)
+                "Port $port on $host is open."
+            }
+        }
+    } catch (e: Exception) {
+        "Port $port on $host is closed or unreachable."
     }
 }
 
@@ -166,17 +221,11 @@ suspend fun executeCommand(command: String): String {
     return withContext(Dispatchers.IO) {
         try {
             val process = Runtime.getRuntime().exec(command)
-            val reader = BufferedReader(InputStreamReader(process.inputStream))
-            val output = StringBuilder()
-            var line: String?
-            while (reader.readLine().also { line = it } != null) {
-                output.append(line).append("\n")
-            }
-            reader.close()
+            val output = process.inputStream.bufferedReader().use { it.readText() }
             process.waitFor()
-            output.toString()
+            output.trim()
         } catch (e: Exception) {
-            "Error executing command: ${e.message}"
+            "Error executing command"
         }
     }
 }
